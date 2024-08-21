99d994d5fbb6f858f282aa4887b80233eb17af80
Joris Bertier <joris.bertier@gmail.com>
2024-08-21 09:42:37 -0400

    done ex

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,25 @@
 import { plantList } from "../datas/plantList"
 function ShoppingList() {
-    console.log(plantList)
+    // console.log(plantList)
+    let categoryPlantList = plantList.map((plant) => plant.category)
+    categoryPlantList = [...new Set(categoryPlantList)]
+    // let categoryPlantList = plantList.reduce((acc, plant) =>
+    //     acc.includes(plant.category) ? acc : acc.concat(plant.category),
+    // []
+    // )
+    console.log(categoryPlantList)
     return (
         <div>
-            {/* {plantList.map((plant, index) => (
-                <li key={`${plant}-${index}`}>{plant}</li>
-            ))} */}
+            <ul>
+                {categoryPlantList.map((category, index) => (
+                    <li key={`${category}-${index}`}>{category}</li>
+                ))}
+            </ul>
+            <ul>
+                {plantList.map((plant, index) => (
+                    <li key={`${plant.name}-${index}`}>{plant.name}</li>
+                ))}
+            </ul>
         </div>
     )
 }
