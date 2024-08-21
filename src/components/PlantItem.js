4a751d2ace22084a715b2374a7f0e9b1b72a166c
Joris Bertier <joris.bertier@gmail.com>
2024-08-21 14:53:51 -0400

    event

--- src/components/PlantItem.js
+++ src/components/PlantItem.js
@@ -2,8 +2,12 @@ import CareScale from "./CareScale"
 import '../styles/PlantItem.css'
 
 function PlantItem({name,cover, id, light, water}) {
+
+    function handleClick(e) {
+        console.log('✨ Ceci est mon event :', e)
+    }
     return (<div>
-                <li key={`${name}-${id}`} className='lmj-plant-item'>
+                <li key={`${name}-${id}`} className='lmj-plant-item' onClick={() => handleClick(name)}>
                 <img className='lmj-plant-item-cover' src={cover} alt={`${name} cover`} />
                     {name}
                     <CareScale careType="water" scaleValue={water} />
