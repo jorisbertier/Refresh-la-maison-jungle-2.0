b7cc19d6b523d8dc008703aa19991088aacdb05f
Joris Bertier <joris.bertier@gmail.com>
2024-08-22 09:05:20 -0400

    add useState cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,19 +1,26 @@
+import { FOCUSABLE_SELECTOR } from '@testing-library/user-event/dist/utils'
 import '../styles/Cart.css'
+import { useState } from 'react'
 
 function Cart() {
     const monsteraPrice = 8
-    const ivyPrice = 10
-    const flowerPrice = 15
+    const [cart, updateCart] = useState(0)
+    // const [open, isOpen] = useState(false)
+
+    function handleCart() {
+        updateCart(cart + 1)
+    }
     return (
-    <div className='lmj-cart'>
-        <h2>Panier</h2>
-        <ul>
-        <li>Monstera : {monsteraPrice}€</li>
-            <li>Lierre : {ivyPrice}€</li>
-            <li>Fleurs : {flowerPrice}€</li>
-        </ul>
-        Total : {monsteraPrice + ivyPrice + flowerPrice }€
-    </div>
+        <div className='lmj-cart'>
+            <h2>Panier</h2>
+            <div>
+                Monstera : {monsteraPrice}€
+                <button onClick={handleCart}>
+                    Ajouter
+                </button>
+            </div>
+            <h3>Total : {monsteraPrice * cart}€</h3>
+        </div>
     )
 }
 
